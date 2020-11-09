fb180a78b8980df54a2940cf73f4e23604e9a813
Lainie Canfield <lainiecanfield@gmail.com>
2020-11-08 23:02:32 -0800

    SQL HW

---
+++ sql-challenge/Employees.sql
@@ -0,0 +1,72 @@
+-- select employee id, first name, last name, sex and salary
+SELECT employees.employee_id, employees.first_name, employees.last_name, employees.sex, salaries.salary
+FROM employees, salaries
+WHERE employees.employee_id = salaries.employee_id;
+
+-- select employees hired in 1986
+
+-- SELECT first_name, last_name, hire_date
+-- TO_CHAR(hire_date, YYYY)
+-- WHERE hire_date = '1986%'
+-- FROM employees;
+
+
+-- SELECT first_name, last_name, date_part('year', hire_date) AS "year"
+-- FROM employees
+-- WHERE hire_date = '1986';
+
+
+-- EXTRACT (YEAR FROM hire_date) AS YEAR
+-- WHERE hire_date = '1986'
+-- FROM employees;
+
+--Manager of each department with dept name and employee information
+SELECT dept_manager.dept_no, departments.dept_name, employees.employee_id, employees.first_name, employees.last_name 
+FROM employees
+JOIN dept_manager
+ON dept_manager.employee_id = employees.employee_id
+JOIN departments
+ON dept_manager.dept_no = departments.dept_no;
+
+--employee information and department name 
+SELECT departments.dept_no, employees.employee_id, employees.first_name, employees.last_name, departments.dept_name
+FROM employees 
+JOIN dept_emp_junction 
+ON employees.employee_id = dept_emp_junction.employee_id
+JOIN departments 
+ON departments.dept_no = dept_emp_junction.dept_no;
+
+
+--employees named Hercules with last name starting with B
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--employees in the sales department
+SELECT employees.employee_id, employees.last_name, employees.first_name, departments.dept_name
+FROM employees 
+JOIN dept_emp_junction 
+ON employees.employee_id = dept_emp_junction.employee_id
+JOIN departments 
+ON departments.dept_no = dept_emp_junction.dept_no
+WHERE departments.dept_no = 'd007';
+
+
+--employees in the sales department and the development department
+SELECT employees.employee_id, employees.last_name, employees.first_name, departments.dept_name
+FROM employees 
+JOIN dept_emp_junction 
+ON employees.employee_id = dept_emp_junction.employee_id
+JOIN departments 
+ON departments.dept_no = dept_emp_junction.dept_no
+WHERE departments.dept_no = 'd007' OR departments.dept_no = 'd005';
+
+SELECT last_name, COUNT(last_name) AS "Last Name Count"
+FROM employees
+GROUP BY last_name
+ORDER BY "last_name" DESC;
+
+
+
+
+
---
+++ sql-challenge/tables.sql
@@ -0,0 +1,65 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/KHGUTs
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+-- employees
+CREATE TABLE "employees" (
+    "employee_id" INT   NOT NULL,
+    "title_id" VARCHAR(30)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "sex" VARCHAR(10)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "employee_id"
+     )
+);
+
+-- departments
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(30)   NOT NULL,
+    "dept_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+-- department number and manager id number
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(30)   NOT NULL,
+    "employee_id" INT   NOT NULL
+);
+
+-- salaries
+CREATE TABLE "salaries" (
+    "employee_id" INT   NOT NULL,
+    "salary" INT   NOT NULL
+);
+
+-- titles
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(30)   NOT NULL,
+    "title" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+-- employee id and department
+CREATE TABLE "dept_emp_junction" (
+    "employee_id" INT   NOT NULL,
+    "dept_no" VARCHAR(30)   NOT NULL
+);
+
+
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_employee_id" FOREIGN KEY("employee_id")
+REFERENCES "employees" ("employee_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_employee_id" FOREIGN KEY("employee_id")
+REFERENCES "employees" ("employee_id");
+
+
+
