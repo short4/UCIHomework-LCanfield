50abb394a68d8af6e77e838d966e7f583d8380eb
Lainie Canfield <lainiecanfield@gmail.com>
2021-01-10 14:30:41 -0800

    HW 1st draft

--- d3-challenge/assets/js/app.js
+++ d3-challenge/assets/js/app.js
@@ -1,1 +1,108 @@
-// @TODO: YOUR CODE HERE!
+// Set chart parameters
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40, 
+    bottom: 60, 
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create SVG container
+var svg = d3.select(".chart")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+//Import data
+d3.csv("..assets.data.csv").then(function(healthData){
+    healthData.forEach(function(data){
+        data.poverty = +data.poverty;
+        data.povertyMoe = +data.povertyMoe;
+        data.age = +data.age;
+        data.ageMoe = +data.ageMoe;
+        data.incomeMoe = +data.incomeMoe;
+        data.healthcare = +data.healthcare;
+        data.healthcareLow = +data.healthcareLow;
+        data.healthcareHigh = +data.healthcareHigh;
+        data.obesity = +data.obesity;
+        data.obesityLow = +data.obesityLow;
+        data.obesityHigh = +data.obesityHigh;
+        data.smokes = +data.smokes;
+        data.smokesLow = +data.smokesHigh;
+    });
+    
+    //create scales
+    var xLinearScale = d3.scaleLinear()
+        .domain([20, d3.max(healthData, d => d.poverty)])
+        .range([0, width]);
+    
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(healthData, d => d.smokes)])
+        .range([height, 0]);
+
+    //create axis
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    //Append to chartGroup
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+    
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    //create circles for scatter plot
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(healthData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.smokes))
+    .attr("cy", d => yLinearScale(d.poverty))
+    .attr("r", "20")
+    .attr("fill", "blue")
+    .attr("opacity", ".5");
+
+    //tooltip
+    var toolTip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([80, -60])
+        .html(function(d){
+            return (`${d.state}`);
+        });
+    
+    chartGroup.call(toolTip);
+
+    circlesGroup.on("click", function(data){
+        toolTip.show(data, this);
+    })
+    //mouseout
+    .on("mouseout", function(data, index){
+        toolTip.hide(data);
+    });
+
+    //axis labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "aText")
+        .text("Poverty Rate");
+
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("class", "aText")
+        .text("Number of Smokers");
+}).catch(function(error){
+    console.log(error);
+});
+
