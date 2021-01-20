ea825cd2270bbd1e7184a58b663a302087ecd472
Lainie Canfield <lainiecanfield@gmail.com>
2021-01-19 21:17:58 -0800

    HW

--- plotly-challenge/StarterCode/static/js/app.js
+++ plotly-challenge/StarterCode/static/js/app.js
@@ -1,74 +1,117 @@
-var select_tag = d3.select("#selDataset");
+var select_id = d3.select("#selDataset");
 
-d3.json("samples.json").then((data) => {
-  var subject_ids =data.names;
+d3.json("samples.json").then((importedData) => {
+  var subject_ids = importedData.names;
 
   console.log("Subject_ids")
   console.log(subject_ids)
+
   subject_ids.forEach((id) => {
-    select_tag
+    select_id
       .append("option")
       .property("value", id)
       .text(id);
   });
+
+  optionChanged(subject_ids[0]);
 });
-// function init(){
-//     data = [{
-//         x:
-//         y:
-//     }]
 
 
-//     var barCHART = d3.selectAll("#bar").node();
+// function when id# is changed
+function optionChanged(selected_id) {
+  console.log("selected_id=", selected_id);
 
-//     Plotly.newPlot(barCHART, data);
-// }
+  d3.json("samples.json").then((data) => {
+  
+    var samples = data.samples;
+    var results = samples.filter(sampleObj => sampleObj.id == selected_id);
 
-// d3.selectAll("selDataset").on("change", updatePlotly);
+    console.log(samples);
 
-// function updatePlotly(){
+    var result = results[0];
 
-//     var dropdownMenu = d3.select("#selDataset");
-//     var dataset = dropdownMenu.node().value;
-// }
-// //  Create the Traces
-//     var trace1 = {
-//       x: data.id,
-//       y: data.otu_ids, 
-//       type: "bar",
-//       orientation: "h"
-//     };
+    console.log(result);
 
-//     // Create the data array for the plot
-//     var data = [trace1];
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
 
-//     // Define the plot layout
-//     var layout = {
-//       title: "OTU's",
-//       xaxis: { title: "OTU ID's" },
-//       yaxis: { title: "Number of OTU" }
-//     };
+    var y_label = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
 
-//     // Plot the chart to a div tag with id "plot"
-//     Plotly.newPlot("bar", data, layout);
+    console.log(sample_values.slice(0, 10).reverse());
 
+    //Create the trace
+    var trace1 = {
+      y: y_label,
+      x: sample_values.slice(0, 10).reverse(),
+      text: otu_labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h",
+    };
 
-//     var trace2 = {
-//         x: data.otu_ids,
-//         y: data.sample_values,
-//         mode: 'markers',
-//         marker: {
-//           color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)']
-//         }
-//       };
+    var data = [trace1];
+
+    var layout = {
+      title: "Top 10 OTUs"
+    };
+
+    //Create bar chart
+    Plotly.newPlot("bar", data, layout);
+
     
-//       var data = [trace2];
-    
-//       var layout = {
-//         title: "OTU's"
-//         showlegend: true,
-//         height: 600,
-//         width: 600
-//       };
-    
-//       Plotly.newPlot('bubble', data, layout);
+    var results = samples.filter(sampleObj => sampleObj.id == selected_id);
+    var result = results[0];
+
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+
+    //Create the trace
+    var trace2 = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Jet"
+      }
+    };
+
+    var data = [trace2];
+
+    var bubble_layout = {
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" }
+    };
+
+    //Create bubble chart
+    Plotly.newPlot("bubble", data, bubble_layout);
+  });
+  
+  //Metadata
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+
+    console.log(metadata);
+
+    var results = metadata.filter(metadataObj => metadataObj.id == selected_id);
+    var result = results[0];
+
+    console.log(result)
+
+    var fig = d3.select("#sample-metadata");
+
+    fig.html("");
+
+    Object.entries(results[0]).forEach(([key, value]) => {
+      fig.append("h5").text(`${key}: ${value}`);
+    });
+
+  });
+}
+
+
+
+
